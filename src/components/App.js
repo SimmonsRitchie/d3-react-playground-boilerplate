ba742e5b8b42f533fc3f0530af73f42f856ad83c
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-22 16:21:05 -0400

    Add style components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Container from "./Container"
+import Svg from "./Svg"
 
 function App() {
   return (
-    <div className="App">
-    <button className="button">Bulma Test</button>
-      <svg width={800} height={800}>
+    <Container>
+      <Svg width={800} height={800}>
         {/* Insert components here */}
-      </svg>
-    </div>
+      </Svg>
+    </Container>
   );
 }
 
