3cf9a672cca7b45ef122d2260f72e9ac7eaaa66f
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-22 15:22:21 -0400

    Add Sass and bulma

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
+    <button className="button">Bulma Test</button>
       <svg width={800} height={800}>
         {/* Insert components here */}
       </svg>
